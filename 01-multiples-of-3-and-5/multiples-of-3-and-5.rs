fde15b83dfd29822d7d3da0129b3605495308345
Jesse Shawl <jesse@jshawl.com>
2014-09-12 16:04:30 -0400

    solve project euler #1

--- 01-multiples-of-3-and-5/multiples-of-3-and-5.rs
+++ 01-multiples-of-3-and-5/multiples-of-3-and-5.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("hello?");
+    let mut count = 0i;
+    let mut sum = 0i;
+    while count < 1000 {
+        if count % 3 == 0 || count % 5 == 0{
+	  sum += count;
+	}
+	count += 1;
+    }
+    println!("{}", sum);
 }
